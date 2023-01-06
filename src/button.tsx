fd5fbc6054c55f796426c87ea4b9954738a86db3
timroberton <timroberton@gmail.com>
2023-01-06 13:06:11 +0800

    First commit

---
+++ src/button.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { cva, VariantProps } from "class-variance-authority";
+
+export const getClassesForButton = cva(
+  "inline-flex items-center justify-center rounded border border-transparent font-400 focus:outline-none",
+  {
+    variants: {
+      intent: {
+        primary:
+          "bg-primary text-primary-content focus:ring-primary  hover:bg-primary-focus",
+        secondary:
+          "bg-secondary text-secondary-content focus:ring-secondary  hover:bg-secondary-focus",
+      },
+      size: {
+        small: "text-sm py-1 px-2",
+        medium: "text-base py-2 px-4",
+      },
+      margin: {
+        left: "ml-2",
+        right: "mr-2",
+      },
+      showFocusedState: {
+        true: "focus:ring-2 focus:ring-offset-2",
+        false: "focus:ring-none",
+      },
+    },
+    // compoundVariants: [
+    //   { intent: "primary", size: "medium", className: "uppercase" },
+    // ],
+    defaultVariants: {
+      intent: "primary",
+      size: "medium",
+      showFocusedState: false,
+    },
+  }
+);
+
+type ButtonProps = React.ComponentPropsWithoutRef<"button"> &
+  VariantProps<typeof getClassesForButton> & {
+    children: React.ReactNode;
+  };
+
+export function Button({
+  className,
+  intent,
+  size,
+  margin,
+  showFocusedState,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      className={getClassesForButton({
+        intent,
+        size,
+        margin,
+        showFocusedState,
+        className,
+      })}
+      {...props}
+    >
+      {props.children}
+    </button>
+  );
+}
---
+++ src/input.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { cva, VariantProps } from "class-variance-authority";
+
+export const getClassesForInput = cva("block w-full rounded border-base-300", {
+  variants: {
+    intent: {
+      primary: "focus:border-primary focus:ring-primary",
+      secondary: "focus:border-secondary focus:ring-primary",
+    },
+    margin: {
+      left: "ml-2",
+      right: "mr-2",
+    },
+  },
+  // compoundVariants: [
+  //   { intent: "primary", size: "medium", className: "uppercase" },
+  // ],
+  defaultVariants: {
+    intent: "primary",
+  },
+});
+
+type InputProps = React.ComponentPropsWithoutRef<"input"> &
+  VariantProps<typeof getClassesForInput>;
+
+export function Input({ className, intent, margin, ...props }: InputProps) {
+  return (
+    <input
+      className={getClassesForInput({
+        intent,
+        margin,
+        className,
+      })}
+      {...props}
+    />
+  );
+}
